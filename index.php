689942d0e13140226bdd1699b25679ff4151bc2c
remysimons <remy.simons@nxp.com>
2016-12-07 08:24:23 +0000

    .

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	</style>
 </head>
 <body>
-	<img id="foto" src="foto.jpg" />
+	<img id="foto" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>Welcome to the beautiful demo webserver of NXP demo, my hostname in docker is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
